0cf6f117e5e66d0dc7be2e83f364f821eaf27272
ostrev <107575015+ostrev@users.noreply.github.com>
2022-11-05 18:46:10 +0200

    Update find_the_capitals.py

--- basic_syntax_conditional_statements_loops_more_exercise/find_the_capitals.py
+++ basic_syntax_conditional_statements_loops_more_exercise/find_the_capitals.py
@@ -1,6 +1,6 @@
-s = input()
-def getindices(s):
-    return [i for i, c in enumerate(s) if c.isupper()]
-
-capt = getindices(s)
-print(capt)
+string_single = list(input())
+result_list = []
+for index in range(len(string_single)):
+    if string_single[index].isupper():
+        result_list.append(index)
+print(result_list)
