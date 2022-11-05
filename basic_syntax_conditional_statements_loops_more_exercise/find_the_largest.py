b6f13a3e04ace588d63c2bff343ccc62da2aa959
ostrev <107575015+ostrev@users.noreply.github.com>
2022-11-05 18:47:20 +0200

    Update find_the_largest.py

--- basic_syntax_conditional_statements_loops_more_exercise/find_the_largest.py
+++ basic_syntax_conditional_statements_loops_more_exercise/find_the_largest.py
@@ -1,14 +1,3 @@
-# number = int(input())
-#
-# large_numb = sorted([i for i in str(number)], reverse=True)
-# large_numb = int(''.join(large_numb))
-# print(large_numb)
-# ------------
-# number = int(input())
-# ще изтриеш част от стойността ако започва с нули. От "00123" ще получиш само 123.
-# Няма нужда да го превръщаш в int след като сортирането е върху цифри(тоест числа между 0 и 9), а не върху числа
-# --------------------
-
 number = list(input())
-number.sort(reverse=True)
-print(''.join(number))
+max_num = sorted(number, reverse=True)
+print(''.join(max_num))
