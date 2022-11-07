682ac4bc1b41f3621e8c6f4daaf742937fccab3b
ostrev <107575015+ostrev@users.noreply.github.com>
2022-11-07 22:46:04 +0200

    Update judge.py

--- dictionaries_more_exercise/judge.py
+++ dictionaries_more_exercise/judge.py
@@ -29,6 +29,6 @@ for contest, user_dict in contests_users_points_dict.items():
 
 counter = 0
 print('Individual standings:')
-for user, points in sorted(individual_standing.items(), key=lambda kvp: (-kvp[1], kvp[0])):
+for user, points in sorted(individual_standing.items(), key=lambda kvpt: (-kvpt[1], kvp[0])):
     counter += 1
     print(f'{counter}. {user} -> {points}')
