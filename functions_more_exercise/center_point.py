395b6b50f5c88db1991286910ea908dccf251eef
ostrev <107575015+ostrev@users.noreply.github.com>
2022-11-19 07:37:36 +0200

    Update center_point.py

--- functions_more_exercise/center_point.py
+++ functions_more_exercise/center_point.py
@@ -1,17 +1,19 @@
 import math
 
 
-def close_to_center(a, b, c, d):
-    l1 = math.sqrt(a**2 + b**2)
-    l2 = math.sqrt(c**2 + d**2)
-    if l1 <= l2:
-        return f'({math.floor(a)}, {math.floor(b)})'
+def find_closest_to_the_center(a, b, c, d):
+    line_one = math.sqrt(a ** 2 + b ** 2)
+    line_tow = math.sqrt(c ** 2 + d ** 2)
+    if line_one <= line_tow:
+        result = f"({math.floor(a)}, {math.floor(b)})"
     else:
-        return f'({math.floor(c)}, {math.floor(d)})'
+        result = f"({math.floor(c)}, {math.floor(d)})"
+    return result
 
 
-x1 = float(input())
-y1 = float(input())
-x2 = float(input())
-y2 = float(input())
-print(close_to_center(x1, y1, x2, y2))
+x_1 = float(input())
+y_1 = float(input())
+x_2 = float(input())
+y_2 = float(input())
+
+print(find_closest_to_the_center(x_1, y_1, x_2, y_2))
