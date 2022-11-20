4facce412da57d9a07e9a2fb61320705fdda38fe
ostrev <107575015+ostrev@users.noreply.github.com>
2022-11-20 12:50:40 +0200

    Update bonnus_scoring_system.py

--- mid_exam/bonnus_scoring_system.py
+++ mid_exam/bonnus_scoring_system.py
@@ -1,20 +1,24 @@
 import math
 
 number_of_students = int(input())
-lectures_num = int(input())
+number_of_lectures = int(input())
 additional_bonus = int(input())
+list_of_bonus = []
+student_attendance = {}
 
+for _ in range(number_of_students):
+    student_attendance[int(input())] = ''
 
-total_bonus = 0
-max_total = 0
-attendance_max = 0
-while number_of_students != 0:
-    attendance = int(input())
-    number_of_students -= 1
-    total_bonus = attendance / lectures_num * (5 + additional_bonus)
-    if total_bonus > max_total:
-        max_total = total_bonus
-        attendance_max = attendance
+for student in student_attendance:
+    total_bonus = student / number_of_lectures * (5 + additional_bonus)
+    student_attendance[student] = math.ceil(total_bonus)
 
-print(f"Max Bonus: {math.ceil(max_total)}.")
-print(f"The student has attended {attendance_max} lectures.")
+if not student_attendance:
+    max_bonus = 0
+    student_attendance[max_bonus] = 0
+
+max_bonus = max(student_attendance)
+
+print(f'Max Bonus: {student_attendance[max_bonus]}.')
+print(f'The student has attended {max_bonus} lectures.')
+
