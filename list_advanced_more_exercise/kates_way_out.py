0d1770d19ece458baa34a467ee647b21f57db080
ostrev <107575015+ostrev@users.noreply.github.com>
2022-11-20 08:29:04 +0200

    Update kates_way_out.py

--- list_advanced_more_exercise/kates_way_out.py
+++ list_advanced_more_exercise/kates_way_out.py
@@ -1,81 +1,56 @@
-def valid_position(row, col, matrix):
-    if 0 <= row < len(matrix) and 0 <= col < len(matrix[row]):
+def valid_position(check_row, check_col, check_matrix):
+    if 0 <= check_row < len(check_matrix) and 0 <= check_col < len(check_matrix[check_row]):
         return True
     return False
 
 
-def next_position(row, col, matrix):
-    if matrix[row][col] == ' ':
+def next_position(next_position_row, next_position_col, next_position_matrix):
+    if next_position_matrix[next_position_row][next_position_col] == ' ':
         return True
     return False
 
 
-def is_last_move(row, col, matrix, size):
-    if row == 0 or row == size - 1 or col == 0 or col == len(matrix[row]) - 1:
+def is_last_move(out_row, out_col, out_matrix, size):
+    if out_row == 0 or out_row == size - 1 or out_col == 0 or out_col == len(out_matrix[out_row]) - 1:
         return True
     return False
 
 
-def find_next_position(row, col, matrix):
-    # row - 1, col - 1
-    # if valid_position(row - 1, col - 1, matrix):
-    #     if next_position(row - 1, col - 1, matrix):
-    #         matrix[row - 1][col - 1] = 'k'
-    #         row = row - 1
-    #         col = col - 1
-    #         return row, col, True
-    # row - 1, col
-    if valid_position(row - 1, col, matrix):
-        if next_position(row - 1, col, matrix):
-            matrix[row - 1][col] = 'k'
-            row = row - 1
-            col = col
-            return row, col, True
-    # row - 1, col + 1
-    # if valid_position(row - 1, col + 1, matrix):
-    #     if next_position(row - 1, col + 1, matrix):
-    #         matrix[row - 1][col + 1] = 'k'
-    #         row = row - 1
-    #         col = col + 1
-    #         return row, col, True
+def find_next_position(find_row, find_col, find_matrix, is_get_out):
+    # row - 1, col UP
+    if valid_position(find_row - 1, find_col, find_matrix):
+        if next_position(find_row - 1, find_col, find_matrix):
+            find_matrix[find_row - 1][find_col] = 'k'
+            next_found_row = find_row - 1
+            next_found_col = find_col
+            is_get_out = True
+            return next_found_row, next_found_col, is_get_out
+    # row, col - 1 LEFT
+    if valid_position(find_row, find_col - 1, find_matrix):
+        if next_position(find_row, find_col - 1, find_matrix):
+            find_matrix[find_row][find_col - 1] = 'k'
+            next_found_row = find_row
+            next_found_col = find_col - 1
+            is_get_out = True
+            return next_found_row, next_found_col, is_get_out
+    # row, col + 1 RIGHT
+    if valid_position(find_row, find_col + 1, find_matrix):
+        if next_position(find_row, find_col + 1, find_matrix):
+            find_matrix[find_row][find_col + 1] = 'k'
+            next_found_row = find_row
+            next_found_col = find_col + 1
+            is_get_out = True
+            return next_found_row, next_found_col, is_get_out
+    # row + 1, col DOWN
+    if valid_position(find_row + 1, find_col, find_matrix):
+        if next_position(find_row + 1, find_col, find_matrix):
+            find_matrix[find_row + 1][find_col] = 'k'
+            next_found_row = find_row + 1
+            next_found_col = find_col
+            is_get_out = True
+            return next_found_row, next_found_col, is_get_out
 
-    # row, col - 1
-    if valid_position(row, col - 1, matrix):
-        if next_position(row, col - 1, matrix):
-            matrix[row][col - 1] = 'k'
-            row = row
-            col = col - 1
-            return row, col, True
-    # row, col + 1
-    if valid_position(row, col + 1, matrix):
-        if next_position(row, col + 1, matrix):
-            matrix[row][col + 1] = 'k'
-            row = row
-            col = col + 1
-            return row, col, True
-
-    # row + 1, col - 1
-    # if valid_position(row + 1, col - 1, matrix):
-    #     if next_position(row + 1, col - 1, matrix):
-    #         matrix[row + 1][col - 1] = 'k'
-    #         row = row + 1
-    #         col = col - 1
-    #         return row, col, True
-    # row + 1, col
-    if valid_position(row + 1, col, matrix):
-        if next_position(row + 1, col, matrix):
-            matrix[row + 1][col] = 'k'
-            row = row + 1
-            col = col
-            return row, col, True
-    # row + 1, col + 1
-    # if valid_position(row + 1, col + 1, matrix):
-    #     if next_position(row + 1, col + 1, matrix):
-    #         matrix[row + 1][col + 1] = 'k'
-    #         row = row + 1
-    #         col = col + 1
-    #         return row, col, True
-    return row, col, False
+    return find_row, find_col, is_get_out
 
 
 number_of_maze = int(input())
@@ -85,34 +60,28 @@ matrix = []
 for line in range(number_of_maze):
     matrix.append(list(input()))
 
-# find Katy's position
-current_position = []
+# find Kate's position
+current_row_position = 0
+current_col_position = 0
 for row in range(number_of_maze):
     for col in range(len(matrix[row])):
         if matrix[row][col] == 'k':
-            current_position.append(row)
-            current_position.append(col)
-
-
-# check position around the Katy
-row = current_position[0]
-col = current_position[1]
+            current_row_position = row
+            current_col_position = col
 
 count = 0
 get_out = False
 while True:
-    next_row, next_col, get_out = find_next_position(row, col, matrix)
+    next_row, next_col, get_out = find_next_position(current_row_position, current_col_position, matrix, get_out)
     count += 1
     if get_out:
-        row = next_row
-        col = next_col
+        current_row_position = next_row
+        current_col_position = next_col
         get_out = False
     else:
-        if is_last_move(row, col, matrix, number_of_maze):
+        if is_last_move(current_row_position, current_col_position, matrix, number_of_maze):
             print(f'Kate got out in {count} moves')
             break
         else:
             print('Kate cannot get out')
             break
-
-
