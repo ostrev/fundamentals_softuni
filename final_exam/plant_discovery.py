f30fae0995fb8d5e698cc9863b469f5bcf50d276
Nikolay Ostrev <107575015+ostrev@users.noreply.github.com>
2022-12-01 11:10:05 +0200

    Update plant_discovery.py

--- final_exam/plant_discovery.py
+++ final_exam/plant_discovery.py
@@ -1,24 +1,20 @@
 line = int(input())
 plants_dictionary = {}
-rating_dictionary = {}
 
 for _ in range(line):
     data = input(). split("<->")
     plant = data[0]
     rarity = int(data[1])
-
     if plant not in plants_dictionary:
         plants_dictionary[plant] = []
         plants_dictionary[plant] = [int(rarity), 0]
     else:
         plants_dictionary[plant][0] = int(rarity)
 
-
 command = input()
 
 while command != 'Exhibition':
     data = command.split(': ')
-
     if data[0] == 'Rate':
         plant_rating = data[1].split(" - ")
         plant = plant_rating[0]
@@ -27,7 +23,6 @@ while command != 'Exhibition':
             print("error")
         else:
             plants_dictionary[plant].append(rating)
-
     elif data[0] == 'Update':
         plant_rating = data[1].split(" - ")
         plant = plant_rating[0]
@@ -36,7 +31,6 @@ while command != 'Exhibition':
             print("error")
         else:
             plants_dictionary[plant][0] = new_rarity
-
     elif data[0] == 'Reset':
         plant = data[1]
         if plant not in plants_dictionary:
@@ -46,7 +40,9 @@ while command != 'Exhibition':
             plants_dictionary[plant] = [int(rarity), 0]
 
     command = input()
+    
 print('Plants for the exhibition:')
+
 for plant, value in plants_dictionary.items():
     rarity = value[0]
     rating_list = value[1::]
