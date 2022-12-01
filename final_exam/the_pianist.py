0e67a78b9038b51b75d0c9a7f5523ccfd65716d2
Nikolay Ostrev <107575015+ostrev@users.noreply.github.com>
2022-12-01 13:47:10 +0200

    Update the_pianist.py

--- final_exam/the_pianist.py
+++ final_exam/the_pianist.py
@@ -1,46 +1,50 @@
-number = int(input())
-pieces = {}
+number_of_pieces = int(input())
+pieces_dictionary = {}
 
-for _ in range(number):
-    piece, composer, key = input().split('|')
-    pieces[piece] = {}
-    pieces[piece]['composer'] = composer
-    pieces[piece]['key'] = key
+for _ in range(number_of_pieces):
+    data = input(). split("|")
+    piece = data[0]
+    composer = data[1]
+    key = data[2]
 
+    if piece not in pieces_dictionary:
+        pieces_dictionary[piece] = []
+    pieces_dictionary[piece] = [composer, key]
 
 command = input()
 
-while command != 'Stop':
-    data = command.split('|')
+while command != "Stop":
+    data = command.split("|")
     if data[0] == 'Add':
         piece = data[1]
         composer = data[2]
         key = data[3]
-        if piece not in pieces:
-            pieces[piece] = {}
-            pieces[piece]['composer'] = composer
-            pieces[piece]['key'] = key
-            print(f'{piece} by {composer} in {key} added to the collection!')
+        if piece in pieces_dictionary:
+            print(f"{piece} is already in the collection!")
         else:
-            print(f'{piece} is already in the collection!')
+            pieces_dictionary[piece] = []
+            pieces_dictionary[piece] = [composer, key]
+            print(f"{piece} by {composer} in {key} added to the collection!")
     elif data[0] == 'Remove':
         piece = data[1]
-        if piece in pieces:
-            del pieces[piece]
-            print(f'Successfully removed {piece}!')
+        if piece in pieces_dictionary:
+            del pieces_dictionary[piece]
+            print(f"Successfully removed {piece}!")
         else:
-            print(f'Invalid operation! {piece} does not exist in the collection.')
+            print(f"Invalid operation! {piece} does not exist in the collection.")
     elif data[0] == 'ChangeKey':
         piece = data[1]
-        new_key = data[2]
-        if piece in pieces:
-            pieces[piece]['key'] = new_key
-            print(f'Changed the key of {piece} to {new_key}!')
+        key = data[2]
+        if piece in pieces_dictionary:
+            print(f"Changed the key of {piece} to {key}!")
+            pieces_dictionary[piece][1] = key
         else:
-            print(f'Invalid operation! {piece} does not exist in the collection.')
+            print(f"Invalid operation! {piece} does not exist in the collection.")
 
     command = input()
 
-sort_pieces = sorted(pieces.items(), key=lambda kvpt: kvpt[0])
-for piece, value in sort_pieces:
-    print(f'{piece} -> Composer: {value["composer"]}, Key: {value["key"]}')
+
+for piece, value in pieces_dictionary.items():
+    composer = value[0]
+    key = value[1]
+    print(f"{piece} -> Composer: {composer}, Key: {key}")
