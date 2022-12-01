9a5d680900c6002b9f65f256e904d50c3a4aad64
Nikolay Ostrev <107575015+ostrev@users.noreply.github.com>
2022-12-01 10:53:34 +0200

    Update plant_discovery.py

--- final_exam/plant_discovery.py
+++ final_exam/plant_discovery.py
@@ -1,66 +1,57 @@
-def check_name(plants, plant):
-    if plant not in plants:
-        print('error')
-        return True
-    return False
+line = int(input())
+plants_dictionary = {}
+rating_dictionary = {}
 
+for _ in range(line):
+    data = input(). split("<->")
+    plant = data[0]
+    rarity = int(data[1])
 
-numbers = int(input())
-
-plants = {}
-for _ in range(numbers):
-    info = input().split('<->')
-    plant = info[0]
-    rarity = int(info[1])
-    plants[plant] = {}
-    plants[plant]['rarity'] = rarity
-
+    if plant not in plants_dictionary:
+        plants_dictionary[plant] = []
+        plants_dictionary[plant] = [int(rarity), 0]
+    else:
+        plants_dictionary[plant][0] = int(rarity)
 
 
 command = input()
 
 while command != 'Exhibition':
     data = command.split(': ')
+
     if data[0] == 'Rate':
-        data_s = data[1].split(' - ')
-        plant = data_s[0]
-        rating = int(data_s[1])
-        if check_name(plants, plant):
-            command = input()
-            continue
-        if 'rating' not in plants[plant]:
-            plants[plant]['rating'] = []
-        plants[plant]['rating'].append(rating)
+        plant_rating = data[1].split(" - ")
+        plant = plant_rating[0]
+        rating = int(plant_rating[1])
+        if plant not in plants_dictionary:
+            print("error")
+        else:
+            plants_dictionary[plant].append(rating)
 
     elif data[0] == 'Update':
-        data_s = data[1].split(' - ')
-        plant = data_s[0]
-        new_rarity = int(data_s[1])
-        if check_name(plants, plant):
-            command = input()
-            continue
-        plants[plant]['rarity'] = new_rarity
+        plant_rating = data[1].split(" - ")
+        plant = plant_rating[0]
+        new_rarity = int(plant_rating[1])
+        if plant not in plants_dictionary:
+            print("error")
+        else:
+            plants_dictionary[plant][0] = new_rarity
+
     elif data[0] == 'Reset':
-        data_s = data[1].split(' - ')
-        plant = data_s[0]
-        if check_name(plants, plant):
-            command = input()
-            continue
-        plants[plant]['rating'].clear()
-                                        # ako dobadq 0 na reitinga move da ne otchita vqrno avr
+        plant = data[1]
+        if plant not in plants_dictionary:
+            print("error")
+        else:
+            rarity = plants_dictionary[plant][0]
+            plants_dictionary[plant] = [int(rarity), 0]
+
     command = input()
-
-for k in plants:
-    if 'rating' not in plants[k]:
-        plants[k]['rating'] = [0]
-    if not plants[k]['rating']:
-        plants[k]['rating'] = [0]
-    a = plants[k]['rating']
-    avr = sum(a)/len(a)
-    plants[k]['rating'] = avr
-
-sort_plants = sorted(plants.items(), key=lambda kvpt: (-kvpt[1]['rarity'], -kvpt[1]['rating']))
 print('Plants for the exhibition:')
-for k, v in sort_plants:
-    print(f'- {k}; Rarity: {v["rarity"]}; Rating: {v["rating"]:.2f}')
-
+for plant, value in plants_dictionary.items():
+    rarity = value[0]
+    rating_list = value[1::]
+    if sum(rating_list) == 0:
+        average = 0
+    else:
+        average = sum(rating_list)/(len(rating_list) - 1)
+    print(f'- {plant}; Rarity: {rarity}; Rating: {average:.2f}')
